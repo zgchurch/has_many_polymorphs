59015fbc61770b936ad48647164090e299520119
Chris Your <chris@ignitionindustries.com>
2010-05-31 11:55:03 -0600

    Added inflections to autoload in Rails 3

--- lib/has_many_polymorphs/railtie.rb
+++ lib/has_many_polymorphs/railtie.rb
@@ -1,7 +1,6 @@
 module HasManyPolymorphs
   class Railtie < Rails::Railtie
     initializer "has_many_polymorphs" do |app|
-      require "#{Rails.root}/config/initializers/inflections"
       require 'has_many_polymorphs/autoload'
       HasManyPolymorphs.setup
     end
